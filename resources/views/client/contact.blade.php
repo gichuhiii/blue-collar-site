b6a9e6bf98a7a368317b2f5c26b4e1d0015223fc
gichuhiii <gnwcocoe@gmail.com>
2022-07-01 20:10:14 +0300

    updated contact

--- resources/views/client/contact.blade.php
+++ resources/views/client/contact.blade.php
@@ -19,15 +19,15 @@
               <div class="contact-item">
                 <i class="fa fa-phone"></i>
                 <h4>Phone</h4>
-                <p>Vivamus ut tellus mi. Nulla nec cursus elit, id vulputate nec cursus augue.</p>
-                <a href="#">+1 333 4040 5566</a>
+                <p>Give us a call.</p>
+                <a href="#">0712345678</a>
               </div>
             </div>
             <div class="col-md-4">
               <div class="contact-item">
                 <i class="fa fa-envelope"></i>
                 <h4>Email</h4>
-                <p>Vivamus ut tellus mi. Nulla nec cursus elit, id vulputate nec cursus augue.</p>
+                <p>Send us an email.</p>
                 <a href="#">contact@company.com</a>
               </div>
             </div>
@@ -35,7 +35,7 @@
               <div class="contact-item">
                 <i class="fa fa-map-marker"></i>
                 <h4>Location</h4>
-                <p>212 Barrington Court New York str <br> USA</p>
+                <p>Nairobi, Kenya<br> AFRICA</p>
                 <a href="#">View on Google Maps</a>
               </div>
             </div>
@@ -49,7 +49,7 @@
             <div class="col-md-12">
               <div class="section-heading">
                 <h2>Send us a <em>message</em></h2>
-                <span>Suspendisse a ante in neque iaculis lacinia</span>
+                <span>something something something</span>
               </div>
             </div>
             <div class="col-md-12">
@@ -96,12 +96,12 @@
       </div>
   
       <div id="map">
-    <!-- How to change your own map point
+    <!-- REMEMBER how to change your own map point
       1. Go to Google Maps
       2. Click on your location point
       3. Click "Share" and choose "Embed map" tab
       4. Copy only URL and paste it within the src="" field below
     -->
-        <iframe src="https://maps.google.com/maps?q=Av.+Lúcio+Costa,+Rio+de+Janeiro+-+RJ,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed" width="100%" height="500px" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d510564.6498664422!2d36.56720029142611!3d-1.3031933719272915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1172d84d49a7%3A0xf7cf0254b297924c!2sNairobi!5e0!3m2!1sen!2ske!4v1656694608352!5m2!1sen!2ske" width="100%" height="500px" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div> 
 @endsection
